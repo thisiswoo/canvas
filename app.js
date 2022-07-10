18fe7520a9e7a0e5a35d1b6d7de5c2a6bd756f72
thisiswoo <thisiswoo0594@gmail.com>
2022-07-10 22:37:55 +0900

    test draw center dot

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+import {Wave} from "./wave.js";
+
 class App {
     constructor() {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
         document.body.appendChild(this.canvas);
+
+        this.wave = new Wave();
 
         window.addEventListener('resize', this.resize.bind(this), fasle);
         this.resize();
@@ -17,10 +21,15 @@ class App {
         this.canvas.width = this.stageWidth * 2;
         this.canvas.height = this.stageHeight * 2;
         this.ctx.scale(2, 2);
+
+        this.wave.resize(this.stageWidth, this.stageHeight);
     }
 
     animate(t) {
         this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+        this.wave.draw(this.ctx);
+
         requestAnimationFrame(this.animate.bind(this));
     }
 }
